f52af0876712e3dec913da663844a72bf150cc0b
chelseaaaaapin <chelseariyantashila06@gmail.com>
2024-12-26 07:55:10 +0700

    Add files via upload

---
+++ handphone.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package handphone;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class handphone {
+    void hidupkan(){
+        System.out.println("HAPE UDAH NYALAAA");
+    }
+    void lakukanPanggilan(){
+            System.out.println("DDRRTTT DDRTTT SEDANG MELAKUKAN PANGGILAN");
+    }
+    void kirimSMS(){
+            System.out.println("TING TING TING WA UDAH DIKIRIIMMM");
+    }
+    void matikan(){
+        System.out.println("HAPEnya UDAH MATIII");
+    }
+}
